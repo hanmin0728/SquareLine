23c7fd20848f2de517306ac90f615497f839c42c
hanmin0728 <gksalsdud1234@gmail.com>
2022-02-08 11:59:07 +0900

    하트시스템 완성

---
+++ Assets/Scripts/Min/Pl.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pl : MonoBehaviour
+{
+    [SerializeField] 
+    private HeartSystem heartSystem;
+    int maxHp = 5;
+    private int curHp=5;
+    private void Start()
+    {
+        heartSystem.Set(maxHp);
+    }
+  
+    public void Damage(int damage)
+    {
+        curHp -= damage;
+        if (curHp > maxHp)
+        {
+            curHp = maxHp;
+        }
+        else if (curHp <= 0)
+        {
+            Dead();
+        }
+        heartSystem.Heart(curHp);
+    }
+    public void Dead()
+    {
+        print("Á×À½");
+    }
+}
